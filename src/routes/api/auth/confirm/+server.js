9d222d54c8bb1224db904c739ef3f75891794d4d
Goran Alković <goranalkovic@users.noreply.github.com>
2023-11-22 23:53:59 +0100

    fix after-login redirect

--- src/routes/api/auth/confirm/+server.js
+++ src/routes/api/auth/confirm/+server.js
@@ -8,7 +8,7 @@ export const GET = async (event) => {
 	} = event;
 	const token_hash = url.searchParams.get('token_hash');
 	const type = url.searchParams.get('type');
-	const nextUrl = url.searchParams.get('next') ?? '';
+	const nextUrl = url.searchParams.get('next') ?? '/';
 
 	if (token_hash && type) {
 		const { data, error } = await supabase.auth.verifyOtp({ token_hash, type });
