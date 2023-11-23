ff50e7e710d072f68755149d18b8a9e39f61b634
Goran Alković <goranalkovic@users.noreply.github.com>
2023-11-23 07:22:36 +0000

    tweak test route

--- src/routes/api/auth/gablec-data/+server.js
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-export const GET = async () => {
-    const url = "https://gableci.hr/vz/";
-    const document = await fetch(url);
-    const rawDocument = await document.text();
-	
-    return rawDocument;
-};
